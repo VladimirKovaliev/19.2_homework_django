fc936bf727ab9abba9bdeeb93393090914a2bfa9
VladimirKovaliev <kovaliev.v@list.ru>
2023-12-26 14:43:17 +0300

    добавляю восстановление пароля

--- users/views.py
+++ users/views.py
@@ -2,19 +2,21 @@ from random import random
 from django.conf import settings
 from django.core.mail import send_mail
 from django.urls import reverse_lazy, reverse
+from django.utils.crypto import get_random_string
 from django.views.generic import CreateView, UpdateView
 from users.models import User
-from users.forms import UserRegisterForm, UserProfileForm
+from users.forms import UserRegisterForm, UserProfileForm, ResetForm
 
 import users
 from django.contrib.auth import logout
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from users.utils import restore_password
 
 
 def logout_view(request):
     logout(request)
     return redirect('/')
+
 
 def generate_new_password(request):
     new_password = ''.join([str(random.randint(0, 9)) for _ in range(12)])
@@ -53,3 +55,26 @@ class ProfileView(UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+
+def reset_password(request):
+    if request.method == 'POST': # если форма отправлена
+        email = request.POST.get('email') # получаем почту из формы
+        user = User.objects.get(email=email) # находим такого пользователя
+        new_password = get_random_string(12) # тут генерируем новый пароль
+        send_mail(
+            subject='Восстановление пароля',
+            message=f'Для входа используйте новый пароль: {new_password}',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[user.email],
+            fail_silently=False,
+        ) # отправлем новый пароль пользователю
+        user.set_password(new_password)
+        user.save()
+        return redirect('users:login')
+    else:
+        form = ResetForm
+        context = {
+            'form': form
+        }
+        return render(request, 'users/reset_password.html', context)
