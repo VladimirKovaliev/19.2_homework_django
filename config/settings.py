f9f90a1e177e200668a781152876be6ee7e6536a
VladimirKovaliev <kovaliev.v@list.ru>
2023-12-28 19:29:38 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -142,6 +142,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 
 EMAIL_HOST_USER = 'kovaliev.v@yandex.ru'
 EMAIL_HOST_PASSWORD = 'fwmbucispjqilbxb'
+DEFAULT_FROM_USER = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
